47007cc2ed9afa6fd9ae308fa8daab0599d6673d
ElenaHrudavenka <kisseli@mail.ru>
2022-02-03 12:14:45 +0300

    Update HW1.tsx

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Message from "./Message";
+import Message from "./Message"
 import s from './Message.module.css'
+import glStyle from './../../../src/p1-main/m1-ui/u1-app/App.module.css'
 
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
+    // avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    avatar: 'https://99px.ru/sstorage/1/2021/06/image_10306212231014024654.gif',
+    name: 'Kisseli',
+    message: 'Крик души! Как же мне не хватает знания верски. Адские муки!!!',
     time: '22:00',
 }
 
@@ -13,7 +15,7 @@ const HW1 = () => {
     return (
         <div className = {s.background}>
             <hr/>
-            homeworks 1
+            <h3 className={glStyle.title}>homeworks 1</h3>
 
             {/*should work (должно работать)*/}
 
