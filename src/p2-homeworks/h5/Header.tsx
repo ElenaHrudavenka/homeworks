caffd45dfd4b937b841b6c293b30f993e9e9221b
ElenaHrudavenka <kisseli@mail.ru>
2022-02-12 13:24:35 +0300

    5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
+import s from './header.module.css'
 
 function Header() {
+    let [visibleMenu, setVisibleMenu] = useState<boolean>(false)
     return (
-       <nav>
-           <NavLink to={'/pre-junior' }>Pre</NavLink>
-           <NavLink to={'/junior' }>Jun</NavLink>
-           <NavLink to={'/junior-plus' }>Jun+</NavLink>
-       </nav>
+        <nav>
+            <img src={require('./img/menu-hamburger-button.png')} alt="Menu" className={s.img}/>
+            <NavLink to={'/pre-junior'} className={s.menu}>Pre</NavLink>
+            <NavLink to={'/junior'} className={s.menu}>Jun</NavLink>
+            <NavLink to={'/junior-plus'} className={s.menu}>Jun+</NavLink>
+        </nav>
     )
 }
 
