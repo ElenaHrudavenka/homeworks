6a5add68ba24f5fb35c6c9ebcd92bebdde04e621
ElenaHrudavenka <kisseli@mail.ru>
2022-02-13 16:29:50 +0300

    update header

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -4,12 +4,17 @@ import s from './header.module.css'
 
 function Header() {
     let [visibleMenu, setVisibleMenu] = useState<boolean>(false)
+    const onClickHandler = () => setVisibleMenu(!visibleMenu)
     return (
-        <nav>
-            <img src={require('./img/menu-hamburger-button.png')} alt="Menu" className={s.img}/>
-            <NavLink to={'/pre-junior'} className={s.menu}>Pre</NavLink>
-            <NavLink to={'/junior'} className={s.menu}>Jun</NavLink>
-            <NavLink to={'/junior-plus'} className={s.menu}>Jun+</NavLink>
+        <nav>-
+            <img src={visibleMenu? require('./img/menu-hamburger-button.png') : require('./img/menu-hamburger-button.png')}
+                 alt="Menu"
+                 className={s.img}
+                 onClick={onClickHandler}
+            />
+            <NavLink to={'/pre-junior'} className={visibleMenu?s.menu:""}>Pre</NavLink>
+            <NavLink to={'/junior'} className={visibleMenu?s.menu:""}>Jun</NavLink>
+            <NavLink to={'/junior-plus'} className={visibleMenu?s.menu:""}>Jun+</NavLink>
         </nav>
     )
 }
