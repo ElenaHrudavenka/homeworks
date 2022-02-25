301ed81bf0384216d3008aa0da37d63c52dd607a
ElenaHrudavenka <kisseli@mail.ru>
2022-02-25 09:59:07 +0300

    update app

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -4,12 +4,12 @@ import HW1 from '../../../p2-homeworks/h1/HW1'
 import HW2 from '../../../p2-homeworks/h2/HW2'
 import HW3 from '../../../p2-homeworks/h3/HW3'
 import HW4 from '../../../p2-homeworks/h4/HW4'
-import HW5 from "../../../p2-homeworks/h5/HW5";
+import HW5 from '../../../p2-homeworks/h5/HW5';
 
 const App = () => {
     return (
         <div className={s.App}>
-            <div>react homeworks:</div>
+            <h3 className={s.title}>react homeworks:</h3>
             {/*<HW1/>
             <HW2/>
             <HW3/>
