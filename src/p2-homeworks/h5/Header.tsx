d60db4d33b9287a5474865a9b896c69c0ca31dee
ElenaHrudavenka <kisseli@mail.ru>
2022-02-25 09:59:21 +0300

    update h5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,21 +1,52 @@
-import React, {useState} from 'react'
+import React, {MouseEventHandler, useState} from 'react'
 import {NavLink} from "react-router-dom";
 import s from './header.module.css'
 
 function Header() {
     let [visibleMenu, setVisibleMenu] = useState<boolean>(false)
     const onClickHandler = () => setVisibleMenu(!visibleMenu)
+
     return (
-        <nav>-
-            <img src={visibleMenu? require('./img/menu-hamburger-button.png') : require('./img/menu-hamburger-button.png')}
-                 alt="Menu"
-                 className={s.img}
-                 onClick={onClickHandler}
-            />
-            <NavLink to={'/pre-junior'} className={visibleMenu?s.menu:""}>Pre</NavLink>
-            <NavLink to={'/junior'} className={visibleMenu?s.menu:""}>Jun</NavLink>
-            <NavLink to={'/junior-plus'} className={visibleMenu?s.menu:""}>Jun+</NavLink>
-        </nav>
+        <div className={s.section}>
+            {/* {visibleMenu &&
+                <nav>
+                    <NavLink to={'/pre-junior'}>
+                        <button>PreJunior</button>
+                    </NavLink>
+                    <NavLink to={'/junior'}>
+                        <button>Junior</button>
+                    </NavLink>
+                    <NavLink to={'/junior-plus'}>
+                        <button>JuniorPlus</button>
+                    </NavLink>
+                </nav>
+            }
+            <a href="#" className={s.menuBtn}>
+                <span></span>
+            </a>*/}
+            {visibleMenu ?
+                <>
+                    <nav>
+                        <NavLink to={'/pre-junior'}>
+                            <button className={s.btnMenu}>PreJunior</button>
+                        </NavLink>
+                        <NavLink to={'/junior'}>
+                            <button className={s.btnMenu}>Junior</button>
+                        </NavLink>
+                        <NavLink to={'/junior-plus'}>
+                            <button className={s.btnMenu}>JuniorPlus</button>
+                        </NavLink>
+                    </nav>
+                    <a href="#" className={s.menuClose} onClick={onClickHandler}>
+                        <span></span>
+                    </a>
+                </> :
+                <a href="#" className={s.menuBtn} onClick={onClickHandler}>
+                    <span></span>
+                </a>
+            }
+
+        </div>
     )
 }
 
